2888cf3bba6e7dce3a0acb7b82b1b22206babbf5
中島浩二 <knaka0209@gmail.com>
2021-01-12 10:49:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+﻿# nextjs_3_4cms
+
+ Version: 0.9.1
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2021/01/12
+
+ update  :
+
+***
+### Summary
+
+Next.js  CMS sample, json file import
+
+***
+### Setup
+
+npx create-next-app app1 --use-npm --example "https://github.com/vercel/next-learn-starter/tree/master/learn-starter"
+
+* npm
+
+npm install dexie --save
+
+***
+### start server
+* Start :
+
+yarn dev
+
+* build:
+
+yarn deploy
+
+
+***
+### Blog :
+
+
+***
+
---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import LibCommon from '../libs/LibCommon'
+import IndexRow from './IndexRow';
+import TopHeadBox from '../components/TopHeadBox'
+//
+//
+export default class Page extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {items: '', items_org: ''}
+//console.log(this.props.data )
+  }
+  async componentDidMount(){
+    var dt = LibCommon.formatDate( new Date(), "YYYY-MM-DD_hhmmss");
+    var url = '/cms.json?' + dt
+console.log(url )
+    const req = await fetch( url );
+    const data = await req.json(); 
+//console.log(data )
+    this.setState({ items: data.items })   
+  } 
+  tabRow(){
+    const items = this.state.items
+    if(items instanceof Array){
+// console.log(items )
+      return items.map((item, index) => {
+        return (<IndexRow key={index}
+          id={item.id} show_id={item.show_id} title={item.title}
+          created_at={item.created_at} />        
+        )
+      })      
+    }
+  } 
+  render() {
+    return (
+    <Layout>
+      <TopHeadBox />
+      <div className="body_main_wrap">
+        <div className="container">
+          <div className="body_wrap">
+            <div id="post_items_box" className="row conte mt-4 mb-4">
+              <div className="col-sm-12">
+                <div id="div_news">
+                  <h2 className="h4_td_title mt-2 mb-2" >Post</h2>
+                </div>
+              </div>
+              {this.tabRow()}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+    )
+  }
+}
