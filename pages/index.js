01209b1286ded8ac4e4b3f5bbb7b11a6c051ee29
中島浩二 <knaka0209@gmail.com>
2021-01-12 15:27:03 +0900

    up,0112a4

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import LibCommon from '../libs/LibCommon'
-import IndexRow from './IndexRow';
 import TopHeadBox from '../components/TopHeadBox'
-//
+import IndexRow from './IndexRow';
+import PagesRow from './PagesRow';
 //
 export default class Page extends React.Component {
   constructor(props){
     super(props)
-    this.state = {items: '', items_org: ''}
+//    this.state = {items: '', items_org: ''}
+    this.state = {items: '', page_items: ''}
 //console.log(this.props.data )
   }
   async componentDidMount(){
@@ -19,9 +20,34 @@ console.log(url )
     const req = await fetch( url );
     const data = await req.json(); 
     var items =  LibCommon.get_reverse_items(data.items)
-// console.log(items )
-    this.setState({ items: items })   
+//console.log(data )
+    this.setState({
+      items: items ,page_items: data.page_items 
+    })   
   } 
+  pageRow(){
+    return this.state.page_items.map(function(item, i){
+//      console.log(item )
+        return <PagesRow save_id={item.save_id} key={i} 
+        title={item.title} />
+    })                            
+  }
+  pageDisp(){
+    if(this.state.page_items instanceof Array){
+      return(
+      <div className="pages_wrap">
+        <div className="row conte mt-0 mb-2">
+          <div className="col-sm-12">
+            <h2 className="h4_td_title mt-2" >Pages</h2>
+            <div className="page_btn_wrap mb-0">
+                {this.pageRow() }
+            </div>
+          </div>
+        </div>
+      </div>        
+      )
+    }
+  }  
   tabRow(){
     const items = this.state.items
     if(items instanceof Array){
@@ -35,13 +61,17 @@ console.log(url )
     }
   } 
   render() {
+//console.log( this.state.page_items)
     return (
     <Layout>
       <TopHeadBox />
       <div className="body_main_wrap">
         <div className="container">
+          <div className="btn_disp_ara_wrap mt-0">
+            {this.pageDisp()}
+          </div>          
           <div className="body_wrap">
-            <div id="post_items_box" className="row conte mt-4 mb-4">
+            <div id="post_items_box" className="row conte mt-2 mb-4">
               <div className="col-sm-12">
                 <div id="div_news">
                   <h2 className="h4_td_title mt-2 mb-2" >Post</h2>
@@ -49,7 +79,7 @@ console.log(url )
               </div>
               {this.tabRow()}
             </div>
-          </div>
+          </div>          
         </div>
       </div>
     </Layout>
