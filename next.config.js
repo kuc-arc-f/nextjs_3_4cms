1b7e445e9947bd90ccb3e97ba7dfbd376e66dd7f
中島浩二 <knaka0209@gmail.com>
2021-01-21 17:20:21 +0900

    up,0121a7

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     CSRF_SECRET : 'secret1234',
-//    BASE_URL: "http://localhost:3000",
-    BASE_URL: "https://next-cms-kuc.netlify.app",
+    BASE_URL: "http://localhost:3000",
+//    BASE_URL: "https://next-cms-kuc.netlify.app",
   },
 }
